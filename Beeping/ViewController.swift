68db86b4159375976f8711d6a9271d96f7f03641
Joseph Levine <joseph.levine@senate.mn>
2019-06-21 12:11:21 -0500

    init

---
+++ Beeping/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beeping
+//
+//  Created by Joseph Levine on 6/21/19.
+//  Copyright © 2019 Flatt Matt Fan Club. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
