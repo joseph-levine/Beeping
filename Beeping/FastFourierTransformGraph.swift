416055e75827586ea54f77ba664b5571a2280bb8
Joseph Levine <adauctus@gmail.com>
2019-08-29 22:14:27 -0500

    initial storyboard setup with a FFT graph and a grid

---
+++ Beeping/FastFourierTransformGraph.swift
@@ -0,0 +1,21 @@
+//
+//  FastFourierTransformGraph.swift
+//  Beeping
+//
+//  Created by Joseph Levine on 8/29/19.
+//  Copyright © 2019 Flatt Matt Fan Club. All rights reserved.
+//
+
+import UIKit
+
+class FastFourierTransformGraph: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
